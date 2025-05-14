bb7f08baccd858d82b4b306e65cb5ce85b5a052e
aqitya <adi.tummala@gmail.com>
2025-05-14 10:23:29 -0700

    Update makingLargeIsland.py

--- questions_and_answers/UF/makingLargeIsland.py
+++ questions_and_answers/UF/makingLargeIsland.py
@@ -5,6 +5,8 @@
 ###
 
 """
+RATING: 1933
+
 You are given an n x n binary matrix grid. You are allowed to change at most one 0 to be 1.
 
 Return the size of the largest island in grid after applying this operation.
